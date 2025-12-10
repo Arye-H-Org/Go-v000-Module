b4c8123d6f49172e09c43b6334ec921509fc33c1
Arye-H-WS <102039251+Arye-H-WS@users.noreply.github.com>
2025-12-10 18:22:53 +0200

    second commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Arye-H-Org/Go-v000-Module
+
+go 1.24.5
---
+++ main.go
@@ -0,0 +1,6 @@
+package v000
+
+// Comment: Dummy function so the module has at least one symbol.
+func Hello() string {
+	return "hello from v0.0.0"
+}
